4282f06338348c19c2acb48524b98562603e4f84
EminemTrungKi <trantatminh1964@gmail.com>
2026-07-15 13:40:10 +0700

    Delete server directory

--- server/routes/projectRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-    const router = express.Router();
-    const { getProjects, createProject } = require("../controllers/projectController");
-
-    router.route("/").get(getProjects).post(createProject);
-
-    module.exports = router;
